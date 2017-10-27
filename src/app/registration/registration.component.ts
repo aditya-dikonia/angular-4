e0bb77ed993eb4104a839822d38c8461f4f31b6e
Vineet Chauhan <vineet.kumar@dikonia.in>
2017-10-27 20:27:23 +0530

    Update Diksha Code

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -12,27 +12,68 @@ export class RegistrationComponent implements OnInit {
   step_2 = false;
   step_3 = false;
   step_4 = false;
+  step_5 = false;
   constructor(private fb:FormBuilder) { 
     this.registrationForm = this.fb.group({
       formStep_1: this.fb.group({
         name: [null, Validators.required],
         email: [null, Validators.email],
-        dob: [null]
+        dob: [null],
+        gender: [null],
+        profile: [null],
+        religion: [null],
+        mothertongue: [null],
+        community: [null],
+        phone: [null],
+        maritalstatus: [null],
+        country: [null],
+        state: [null],
+        city: [null]
       }),
       formStep_2: this.fb.group({
-        name: [null],
-        email: [null],
-        dob: [null]
+        education_level: [null],
+        education_field: [null],
+        colleges_attended: [null],
+        work: [null],
+        designation: [null],
+        employer_name: [null],
+        annual_income: [null]
       }),
       formStep_3: this.fb.group({
-        name: [null],
-        email: [null],
-        dob: [null]
+        chant: [null],
+        councelling: [null],
+        initiated: [null],
+        spiritual_master: [null],
+        principles: [null],
+        active_service: [null],
+        temple_associated: [null],
+        leader: [null]
       }),
       formStep_4: this.fb.group({
-        name: [null],
-        email: [null],
-        dob: [null]
+        height: [null],
+        body_type: [null],
+        skin_type: [null],
+        mobile_number: [null],
+        about: [null],
+        disability: [null],
+        photo: [null]
+      }),
+      formStep_5: this.fb.group({
+        partner_age: [null],
+        partner_height: [null],
+        partner_maritalstatus: [null],
+        partner_mothertongue: [null],
+        partner_spiritual_master: [null],
+        partner_spiritual_location: [null],
+        partner_spiritual_status: [null],
+        partner_country: [null],
+        partner_state: [null],
+        partner_education: [null],
+        partner_working_with: [null],
+        partner_profession_area: [null],
+        partner_annual_income: [null],
+        partner_profile_created: [null],
+        partner_diet: [null]
       }),
     });
   }
@@ -47,6 +88,7 @@ export class RegistrationComponent implements OnInit {
     this.step_2 = false;
     this.step_3 = false;
     this.step_4 = false;
+    this.step_5 = false;
     switch(step){
       case 1:
         this.step_1 = true;
@@ -59,6 +101,9 @@ export class RegistrationComponent implements OnInit {
         break;
       case 4:
         this.step_4 = true;
+        break;
+      case 5:
+        this.step_5 = true;
         break;
     }
   }
